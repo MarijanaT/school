c7259beff816d8de2397d0cfa2e1b12f736a376c
Marijana Tešić <tesic.marijana@gmail.com>
2017-10-08 16:42:23 +0200

    psql

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,5 @@
 class PagesController < ApplicationController
+  def last_tasks
+    @task='bla bla'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :tasks
   root to:'tasks#index'
   get 'ordinacija', to:'pages#index'
-
+  get 'contact', to:'pages#contact'
+  get 'last_tasks', to:'pages#last_tasks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
